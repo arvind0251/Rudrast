34bfe4578d0c5324c5f19fff51fff77f413698e3
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2024-11-25 07:35:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", 21552265))
+API_HASH = getenv("API_HASH", 1c971ae7e62cc416ca977e040e700d09)
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", 7133864474:AAFe2hkW6Xts85GLK9ZbQQdrRiHrtiNqbgM)
+MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority)
 
 OWNER_ID = int(getenv("OWNER_ID", 7408008545))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+ekvvqYVRa6c0YmI1")
